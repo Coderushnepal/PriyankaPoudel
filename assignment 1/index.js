2ae24fad6fe7dc1b77509320ec10c0d0847e6427
Priyanka Poudel <priyankapaudel20@gmail.com>
2022-01-03 23:07:23 -0800

    Ability test

---
+++ assignment 1/index.js
@@ -0,0 +1,8 @@
+var num;
+for(num=1; num<100; num++){
+    if ((num%3)==0)
+    // console.log("fizz")
+    // else(console.log(num));
+    document.write("<br>"+"fizz")
+    else document.write("<br>" +num);
+}
