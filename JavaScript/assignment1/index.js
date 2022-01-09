78e1baf50c3a46f375d3c7bab5b0a212620ffbcb
Priyanka Poudel <priyankapaudel20@gmail.com>
2022-01-08 22:26:46 -0800

    assignment

--- JavaScript/assignment1/index.js
+++ JavaScript/assignment1/index.js
@@ -1,8 +1,27 @@
-var num;
-for(num=1; num<100; num++){
-    if ((num%3)==0)
-    // console.log("fizz")
-    // else(console.log(num));
-    document.write("<br>"+"fizz")
-    else document.write("<br>" +num);
+// var num;
+// for(num=1; num<100; num++){
+//     if ((num%3)==0)
+//     // console.log("fizz")
+//     // else(console.log(num));
+//     document.write("<br>"+"fizz")
+//     else document.write("<br>" +num);
+// }
+
+// 1 answer
+
+function giveMeSomething (text){
+    return ("something" + text);
 }
+console.log(giveMeSomething(" is better than nothing"));
+console.log(giveMeSomething(" Bob Jane"));
+console.log( giveMeSomething(" something"));
+
+//2 answer
+
+function newWord(text){
+  return (text.substr(1));
+}
+console.log(newWord("apple"));
+console.log(newWord("cherry"));
+console.log(newWord("plum"));
+
---
+++ JavaScript/javascript basic/index.js
@@ -0,0 +1,15 @@
+
+
+var num = 6;
+var output="*";
+for(i=6; i<=num; i--){
+  console.log(output.repeat(i));
+}
+
+// function starPattern(num){
+//   for(i=6; i<=num; i--){
+//     console.log("*".repeat(i))
+//   }
+//   return(num(6));
+// }
+
